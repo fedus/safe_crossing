1d8c888bd866ab1bda6d5c9816452ff0d4ebbe78
Federico Gentile <gentfede+git@gmail.com>
2021-06-08 23:49:02 +0200

    feat: refactor action buttons

--- lib/pages/swipe_screen.dart
+++ lib/pages/swipe_screen.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
+import 'package:safe_crossing/widgets/action_buttons.dart';
 import 'package:safe_crossing/widgets/crossing_infos.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:swipable_stack/swipable_stack.dart';
@@ -240,81 +240,17 @@ class _SwipeScreenState extends State<SwipeScreen> {
                     },
                   ),
                 ),
-                SafeArea(child: Stack(children: [Positioned(
-                    top: 20,
-                    right: 20,
-                    child: FloatingActionButton(
-                      child: Icon(Icons.help),
-                      backgroundColor: Colors.blue,
-                      heroTag: 1,
-                      onPressed: _showHelpDialog,
-                    )),
-                  Positioned(
-                      top: 100,
-                      right: 20,
-                      child: FloatingActionButton(
-                        child: Icon(Icons.streetview),
-                        backgroundColor: Colors.green,
-                        heroTag: 2,
-                        onPressed: _openStreetViewUrl,
-                      )),
-                  Positioned(
-                      top: 180,
-                      right: 20,
-                      child: Visibility(
-                          visible: percentPlaceholder < 100,
-                          child: SizedBox(width: 60, height: 60, child: Material(type: MaterialType.circle, color: Colors.blue.withAlpha(1), elevation: 10, child: PieChart(
-                            PieChartData(
-                                borderData: FlBorderData(
-                                  show: false,
-                                ),
-                                sectionsSpace: 0,
-                                centerSpaceRadius: 15,
-                                sections: [
-                                  PieChartSectionData(
-                                    color: Colors.green,
-                                    value: percentOk,
-                                    showTitle: false,
-                                    radius: 10,
-                                  ),
-                                  PieChartSectionData(
-                                    color: Colors.orange,
-                                    value: percentNotSure,
-                                    showTitle: false,
-                                    radius: 10,
-                                  ),
-                                  PieChartSectionData(
-                                    color: Colors.red,
-                                    value: percentTooClose,
-                                    showTitle: false,
-                                    radius: 10,
-                                  ),
-                                  PieChartSectionData(
-                                    color: Colors.white60,
-                                    value: percentTie,
-                                    radius: 10,
-                                    showTitle: false,
-                                  ),
-                                  PieChartSectionData(
-                                    color: const Color(0xff0293ee).withAlpha(1),
-                                    value: percentPlaceholder,
-                                    radius: 10,
-                                    showTitle: false,
-                                  ),
-                                ]),
-                          )),
-                          ))),
-                  Positioned(
-                      top: percentPlaceholder < 100 ? 260 : 180,
-                      right: 20,
-                      child: Visibility(
-                          visible: swipeController.canRewind,
-                          child: FloatingActionButton(
-                            child: Icon(Icons.undo),
-                            backgroundColor: Colors.orange,
-                            heroTag: 3,
-                            onPressed: () => swipeController.rewind(),
-                          )))])),
+                SafeArea(child: ActionButtons(
+                  helpCallback: _showHelpDialog,
+                  streetViewCallback: _openStreetViewUrl,
+                  rewindCallback: swipeController.rewind,
+                  canRewind: swipeController.canRewind,
+                  percentOk: percentOk,
+                  percentNotSure: percentNotSure,
+                  percentTooClose: percentTooClose,
+                  percentTie: percentTie,
+                  percentPlaceholder: percentPlaceholder
+                )),
               ]);
             }
 
---
+++ lib/widgets/action_buttons.dart
@@ -0,0 +1,108 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ActionButtons extends StatelessWidget {
+  final VoidCallback helpCallback;
+  final VoidCallback streetViewCallback;
+  final VoidCallback rewindCallback;
+  final bool canRewind;
+
+  final double percentOk;
+  final double percentNotSure;
+  final double percentTooClose;
+  final double percentTie;
+  final double percentPlaceholder;
+
+
+  ActionButtons({
+    @required this.helpCallback,
+    @required this.streetViewCallback,
+    @required this.rewindCallback,
+    @required this.canRewind,
+    @required this.percentOk,
+    @required this.percentNotSure,
+    @required this.percentTooClose,
+    @required this.percentTie,
+    @required this.percentPlaceholder
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [Positioned(
+        top: 20,
+        right: 20,
+        child: FloatingActionButton(
+          child: Icon(Icons.help),
+          backgroundColor: Colors.blue,
+          heroTag: 1,
+          onPressed: helpCallback,
+        )),
+      Positioned(
+          top: 100,
+          right: 20,
+          child: FloatingActionButton(
+            child: Icon(Icons.streetview),
+            backgroundColor: Colors.green,
+            heroTag: 2,
+            onPressed: streetViewCallback,
+          )),
+      Positioned(
+          top: 180,
+          right: 20,
+          child: Visibility(
+              visible: percentPlaceholder < 100,
+              child: SizedBox(width: 60, height: 60, child: Material(type: MaterialType.circle, color: Colors.blue.withAlpha(1), elevation: 10, child: PieChart(
+                PieChartData(
+                    borderData: FlBorderData(
+                      show: false,
+                    ),
+                    sectionsSpace: 0,
+                    centerSpaceRadius: 15,
+                    sections: [
+                      PieChartSectionData(
+                        color: Colors.green,
+                        value: percentOk,
+                        showTitle: false,
+                        radius: 10,
+                      ),
+                      PieChartSectionData(
+                        color: Colors.orange,
+                        value: percentNotSure,
+                        showTitle: false,
+                        radius: 10,
+                      ),
+                      PieChartSectionData(
+                        color: Colors.red,
+                        value: percentTooClose,
+                        showTitle: false,
+                        radius: 10,
+                      ),
+                      PieChartSectionData(
+                        color: Colors.white60,
+                        value: percentTie,
+                        radius: 10,
+                        showTitle: false,
+                      ),
+                      PieChartSectionData(
+                        color: const Color(0xff0293ee).withAlpha(1),
+                        value: percentPlaceholder,
+                        radius: 10,
+                        showTitle: false,
+                      ),
+                    ]),
+              )),
+              ))),
+      Positioned(
+          top: percentPlaceholder < 100 ? 260 : 180,
+          right: 20,
+          child: Visibility(
+              visible: canRewind,
+              child: FloatingActionButton(
+                child: Icon(Icons.undo),
+                backgroundColor: Colors.orange,
+                heroTag: 3,
+                onPressed: rewindCallback,
+              )))]);
+  }
+}
