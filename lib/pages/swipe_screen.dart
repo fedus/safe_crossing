e4752285a918cba79b6acebfcc8064963f108798
Federico Gentile <gentfede+git@gmail.com>
2021-06-29 18:32:25 +0200

    feat: improve imagery switch toast placement

--- lib/pages/swipe_screen.dart
+++ lib/pages/swipe_screen.dart
@@ -190,14 +190,17 @@ class _SwipeScreenState extends State<SwipeScreen> {
     }
 
     ftoast.showToast(
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25.0),
-          color: Colors.white54,
-        ),
-        child: Text(toastText),
-      ),
+      child: Padding(
+          padding: EdgeInsets.only(top: 400),
+          child: Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              color: Colors.white54,
+            ),
+            child: Text(toastText),
+          )),
       gravity: ToastGravity.CENTER,
       toastDuration: Duration(seconds: 2),
     );
